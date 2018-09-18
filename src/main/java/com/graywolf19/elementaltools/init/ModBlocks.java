ee80a5191f42b1e348cbe35738d5457b951b8e2f
GrayWolf19 <pianodude919@yahoo.com>
2018-09-18 17:09:00 -0400

    Added elemental ingots and respective blocks

--- src/main/java/com/graywolf19/elementaltools/init/ModBlocks.java
+++ src/main/java/com/graywolf19/elementaltools/init/ModBlocks.java
@@ -3,7 +3,7 @@ package com.graywolf19.elementaltools.init;
 import java.util.ArrayList;
 import java.util.List;
 
-//import com.graywolf19.elementaltools.blocks.BlockBase;
+import com.graywolf19.elementaltools.blocks.BlockBase;
 import com.graywolf19.elementaltools.blocks.CrystalOreBase;
 
 import net.minecraft.block.Block;
@@ -13,6 +13,7 @@ public class ModBlocks
 {
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
 	
+	//-------------------Crystal Ores-------------------//
 	public static final Block AMETHYST_ORE = new CrystalOreBase("amethyst_ore", Material.ROCK);
 	public static final Block AQUAMARINE_ORE = new CrystalOreBase("aquamarine_ore", Material.ROCK);
 	public static final Block CITRINE_ORE = new CrystalOreBase("citrine_ore", Material.ROCK);
@@ -23,4 +24,18 @@ public class ModBlocks
 	public static final Block SAPPHIRE_ORE = new CrystalOreBase("sapphire_ore", Material.ROCK);
 	public static final Block TOPAZ_ORE = new CrystalOreBase("topaz_ore", Material.ROCK);
 	public static final Block WHITE_QUARTZ_ORE = new CrystalOreBase("white_quartz_ore", Material.ROCK);
+	//--------------------------------------------------//
+	
+	//-------------------Metal Blocks-------------------//
+	public static final Block SHADOW_METAL_BLOCK = new BlockBase("shadow_metal_block", Material.IRON);
+	public static final Block ICE_METAL_BLOCK = new BlockBase("ice_metal_block", Material.IRON);
+	public static final Block EARTH_METAL_BLOCK = new BlockBase("earth_metal_block", Material.IRON);
+	public static final Block MAGNET_METAL_BLOCK = new BlockBase("magnet_metal_block", Material.IRON);
+	public static final Block LIGHT_METAL_BLOCK = new BlockBase("light_metal_block", Material.IRON);
+	public static final Block GRASS_METAL_BLOCK = new BlockBase("grass_metal_block", Material.IRON);
+	public static final Block FIRE_METAL_BLOCK = new BlockBase("fire_metal_block", Material.IRON);
+	public static final Block WATER_METAL_BLOCK = new BlockBase("water_metal_block", Material.IRON);
+	public static final Block ELECTRIC_METAL_BLOCK = new BlockBase("electric_metal_block", Material.IRON);
+	public static final Block AIR_METAL_BLOCK = new BlockBase("air_metal_block", Material.IRON);
+	//--------------------------------------------------//
 }
--- src/main/java/com/graywolf19/elementaltools/init/ModItems.java
+++ src/main/java/com/graywolf19/elementaltools/init/ModItems.java
@@ -24,5 +24,16 @@ public class ModItems
 	public static final Item WHITE_QUARTZ = new ItemBase("white_quartz");
 	//----------------------------------------------//
 	
-	
+	//-------------------Metals-------------------//
+	public static final Item SHADOW_METAL_INGOT = new ItemBase("shadow_metal_ingot");
+	public static final Item ICE_METAL_INGOT = new ItemBase("ice_metal_ingot");
+	public static final Item EARTH_METAL_INGOT = new ItemBase("earth_metal_ingot");
+	public static final Item MAGNET_METAL_INGOT = new ItemBase("magnet_metal_ingot");
+	public static final Item LIGHT_METAL_INGOT = new ItemBase("light_metal_ingot");
+	public static final Item GRASS_METAL_INGOT = new ItemBase("grass_metal_ingot");
+	public static final Item FIRE_METAL_INGOT = new ItemBase("fire_metal_ingot");
+	public static final Item WATER_METAL_INGOT = new ItemBase("water_metal_ingot");
+	public static final Item ELECTRIC_METAL_INGOT = new ItemBase("electric_metal_ingot");
+	public static final Item AIR_METAL_INGOT = new ItemBase("air_metal_ingot");
+	//----------------------------------------------//
 }
